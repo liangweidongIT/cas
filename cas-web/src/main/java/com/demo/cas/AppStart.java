47b317257e5bd60cf7ef5287effafe480f3b0585
liangwd_local <liangweidongIT@163.com>
2018-12-10 09:31:17 +0800

    版本回退

--- cas-web/src/main/java/com/demo/cas/AppStart.java
+++ cas-web/src/main/java/com/demo/cas/AppStart.java
@@ -17,18 +17,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class AppStart {
 	public static void main(String[] args) {
 		SpringApplication.run(AppStart.class);
-		log.info("程序启动！！！");
-		log.info("可以");
-		System.out.println("hello git");
-		System.out.println("update from git");
-		log.info("udate from local");
-		log.info("update from git 1111111");
-		log.info("update from loca11111");
-		log.info("update from loca222222");
-		log.info("update from local3333333");
-		log.info("update from git222222");
-		log.info("update from local_444444");
-		log.info("update from git_333333");
 	}
 
 }
